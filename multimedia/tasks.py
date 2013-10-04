365ba6fddedde8f29258f5b7d84fd900be2077f5
Jason Bittel <jason.bittel@gmail.com>
2013-10-04 09:39:34 -0700

    Update tasks with Celery 3.0 changes

--- multimedia/tasks.py
+++ multimedia/tasks.py
@@ -1,19 +1,22 @@
 from celery.decorators import task
 from celery.task.sets import subtask
+from celery.utils.log import get_task_logger
 
 from models import MediaBase, Video
+
+
+logger = get_task_logger(__name__)
 
 
 @task(max_retries=3)
 def generate_thumbnail(video_id, callback=None):
     media = Video.objects.get(pk=video_id)
-    logger = generate_thumbnail.get_logger()
     logger.info("Generating thumbnail for %s" % media)
     try:
         media.make_thumbnail()
     except Exception, exc:
         logger.info("Generate thumbnail failed for %s - retrying " % media)
-        generate_thumbnail.retry(exc=exc, countdown=60)
+        raise generate_thumbnail.retry(exc=exc, countdown=60)
     logger.info("Done generating thumbnail for %s" % media)
     if callback:
         subtask(callback).delay(media.id)
@@ -23,13 +26,12 @@ def generate_thumbnail(video_id, callback=None):
 def encode_media(media_id, callback=None):
     base = MediaBase.objects.get(pk=media_id)
     media = base.get_media()
-    logger = encode_media.get_logger()
     logger.info("Encoding %s" % media)
     try:
         media.encode_file()
-    except Exception, exc:
+    except Exception as exc:
         logger.info("Encode Media failed for %s - retrying " % media)
-        encode_media.retry(exc=exc, countdown=60)
+        raise encode_media.retry(exc=exc, countdown=60)
     logger.info("Done encoding for %s" % media)
     logger.info("Saving model and starting upload for %s" % media)
     if callback:
@@ -40,11 +42,10 @@ def encode_media(media_id, callback=None):
 def upload_media(media_id):
     base = MediaBase.objects.get(pk=media_id)
     media = base.get_media()
-    logger = upload_media.get_logger()
     logger.info("Uploading encoded file for %s" % media)
     try:
         media.upload_file()
-    except Exception, exc:
+    except Exception as exc:
         logger.info("Upload media failed for %s - retrying " % media)
-        upload_media.retry(exc=exc, countdown=60)
+        raise upload_media.retry(exc=exc, countdown=60)
     logger.info("Upload complete for %s" % media)
