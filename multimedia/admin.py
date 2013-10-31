68d4adcfc05f7d39bbb086e2b159b6f6d980ea76
Jason Bittel <jason.bittel@gmail.com>
2013-10-31 13:28:19 -0700

    Enable translation for admin strings

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy
 
 from .models import Audio
 from .models import Video
@@ -15,20 +18,22 @@ class MediaAdmin(admin.ModelAdmin):
         if not change:
             obj.owner = request.user
         obj.save()
+        meta = self.model._meta
         if not obj.encoded:
-            self.message_user(request, "Your file is being encoded. An email notification will be sent when complete.")
+            self.message_user(request, _("Your %s is being encoded. An email notification will be sent when complete.") % force_text(meta.verbose_name))
 
     def re_encode(self, request, queryset):
         for media in queryset:
             media.encoded = False
             media.save()
             media.encode()
+        meta = self.model._meta
         if len(queryset) == 1:
-            message_bit = "file is"
+            message_bit = "%s is" % force_text(meta.verbose_name)
         else:
-            message_bit = "files are"
-        self.message_user(request, "Your %s being encoded. An email notification will be sent when complete." % message_bit)
-    re_encode.short_description = "Re-encode selected %(verbose_name_plural)s"
+            message_bit = "%s are" % force_text(meta.verbose_name_plural)
+        self.message_user(request, _("Your %s being encoded. An email notification will be sent when complete.") % message_bit)
+    re_encode.short_description = ugettext_lazy("Re-encode selected %(verbose_name_plural)s")
 
 
 class VideoAdmin(MediaAdmin):
