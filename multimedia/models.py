beb012e34ff8eeef8ad91617b21c3cf695687e1a
Jason Bittel <jason.bittel@gmail.com>
2013-11-06 11:39:37 -0800

    Fix re-encode on file comparison

--- multimedia/models.py
+++ multimedia/models.py
@@ -25,9 +25,9 @@ from celery import chord
 from filer.fields.image import FilerImageField
 
 from .conf import multimedia_settings
-from .signals import check_file_changed
+from .signals import set_encode_profiles
+from .signals import encode_profiles_changed
 from .signals import thumbnail_offset_changed
-from .signals import encode_profile_changed
 from .storage import OverwritingStorage
 
 
@@ -181,6 +181,7 @@ class Audio(MediaBase):
         verbose_name_plural = "Audio Files"
 
 
-pre_save.connect(check_file_changed)
+pre_save.connect(set_encode_profiles, sender=Video)
+pre_save.connect(set_encode_profiles, sender=Audio)
 pre_save.connect(thumbnail_offset_changed, sender=Video)
-m2m_changed.connect(encode_profile_changed, sender=MediaBase.profiles.through)
+m2m_changed.connect(encode_profiles_changed, sender=MediaBase.profiles.through)
--- multimedia/signals.py
+++ multimedia/signals.py
@@ -4,48 +4,39 @@ import os
 from django.conf import settings
 
 
-def check_file_changed(sender, instance, **kwargs):
+def set_encode_profiles(sender, instance, **kwargs):
     """
     Signal: pre_save
+    Sender: Video, Audio
 
-    Prior to saving, check to see if the uploaded file is different
-    than the existing file. If so, set it to be encoded.
+    Prior to saving, store the currently configured encoding profiles
+    so we can later detect if the file needs to be encoded. If a new
+    file is being uploaded, force the re-encode.
     """
-    if instance.id and hasattr(instance.file.file, "temporary_file_path"):
-        current = sender.objects.get(pk=instance.id)
-        if not cmp(os.path.join(settings.MEDIA_ROOT, current.file.name),
-                   os.path.join(settings.MEDIA_ROOT, instance.file.file.temporary_file_path())):
-            instance.encoded = False
-        current.file.delete(save=False)
-
-    # Store the currently configured encoding profiles
-    # so we can later detect if they have changed
+    # Store the currently configured encoding profiles so we can
+    # compare against them later
     if instance.pk:
         instance._profiles = set(list(instance.profiles.values_list('pk', flat=True)))
     else:
         instance._profiles = set(list())
 
-
-def thumbnail_offset_changed(sender, instance, **kwargs):
-    """
-    Signal: pre_save
-    Sender: Video
-
-    If the thumbnail offset has changed, generate a new one with the
-    changed offset value.
-    """
-    from .tasks import generate_thumbnail
-    try:
-        current = sender.objects.get(pk=instance.pk)
-    except sender.DoesNotExist:
-        pass
-    else:
-        if current.auto_thumbnail_offset != instance.auto_thumbnail_offset:
-            generate_thumbnail.apply_async((instance.model_name, current.pk),
-                                           countdown=5)
+    # If a new file is being uploaded and it's different than the
+    # existing file, set it to be encoded
+    if hasattr(instance.file.file, 'temporary_file_path'):
+        try:
+            current = sender.objects.get(pk=instance.pk)
+        except sender.DoesNotExist:
+            pass
+        else:
+            current_file = os.path.join(settings.MEDIA_ROOT, current.file.name)
+            new_file = os.path.join(settings.MEDIA_ROOT,
+                                    instance.file.file.temporary_file_path())
+            if not cmp(current_file, new_file):
+                instance._profiles = set(list())
+            current.file.delete(save=False)
 
 
-def encode_profile_changed(sender, instance, action, **kwargs):
+def encode_profiles_changed(sender, instance, action, **kwargs):
     """
     Signal: m2m_changed
     Sender: MediaBase.profiles.through
@@ -58,3 +49,22 @@ def encode_profile_changed(sender, instance, action, **kwargs):
         profiles = set(kwargs['pk_set'])
         if not instance.encoding and instance._profiles != profiles:
             instance.encode()
+
+
+def thumbnail_offset_changed(sender, instance, **kwargs):
+    """
+    Signal: pre_save
+    Sender: Video
+
+    If the thumbnail offset has changed, regenerate it with the
+    changed offset value.
+    """
+    from .tasks import generate_thumbnail
+    try:
+        current = sender.objects.get(pk=instance.pk)
+    except sender.DoesNotExist:
+        pass
+    else:
+        if current.auto_thumbnail_offset != instance.auto_thumbnail_offset:
+            generate_thumbnail.apply_async((instance.model_name, current.pk),
+                                           countdown=5)
