eb80699e6aadd5cba273c06f113597df0cca2eb2
Jason Bittel <jason.bittel@gmail.com>
2013-11-15 16:40:03 -0800

    Update setup script

--- setup.py
+++ setup.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 from multimedia import __version__ as version
 
 
+packages = [
+    'multimedia',
+    'multimedia.conf',
+    'multimedia.migrations',
+]
+
+package_data = {
+    '': ['AUTHORS', 'LICENSE', 'README.rst'],
+    'multimedia': ['fixtures/*.json',
+                   'templates/multimedia/*.html', 'templates/multimedia/*.txt'],
+}
+
 install_requires = [
-    'setuptools',
     'django-celery',
     'django-filer',
     'paramiko',
 ]
 
+with open('README.rst') as f:
+    readme = f.read()
 
 setup(
     name='django-multimedia',
     version=version,
     url='http://github.com/jbittel/django-multimedia',
     license='BSD',
-    description='',
+    description='A Django app for encoding and uploading video within the '
+                'Django admin. Supports any video profile you can write a '
+                'command line statement to accomplish.',
+    long_description=readme,
     author='Jason Bittel',
     author_email='jason.bittel@gmail.com',
     package_dir={'multimedia': 'multimedia'},
-    packages=find_packages(),
+    packages=packages,
     install_requires=install_requires,
     include_package_data=True,
     zip_safe=False,
